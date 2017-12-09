150b4840da21fce8ffe8f681c6de2fcf11d062af
clemente <info@blablakasa.com>
2017-12-09 21:02:36 +0000

    asi ponemos el php cpn el html

---
+++ tema_3_integracion_con_html.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>HTML5</title>
+    <link rel="stylesheet" href="css/master.css">
+  </head>
+  <body>
+
+<footer>&copy;copyright
+<?php
+echo "by clemente";
+ ?>
+
+            </footer>
+  </body>
+</html>
