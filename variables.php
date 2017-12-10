73c2085dda85bdf479ece0c0724be26852017a99
clemente <info@blablakasa.com>
2017-12-10 14:24:50 +0000

    variables

---
+++ variables.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title><?php $titulo ?></title>
+  </head>
+  <body>
+    
+      <?php
+        $nombre = "clemente"; //string = cadena de texto"
+          $apellidos = "Alvarez";
+    /* con echo comillas +variable+un espacio separamos
+    separamos los resultados ede echo*/
+            echo "$nombre ", "$apellidos";
+
+   ?>
+
+
+  </body>
+</html>
